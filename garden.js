38630735db6c5e5f64280beea97da1c7b1a7dbc8
Kent Roth <kentencroth@yahoo.com>
2021-04-11 07:29:21 -0500

    Setup

---
+++ garden.js
@@ -0,0 +1,11 @@
+var Gpio = require('onoff').Gpio;
+
+// 4 moisture sensors and 4 motors.
+
+// function to get info from sensors
+
+// function to togle motors
+
+// only 1 motor running at a time
+
+// needs to have a fail safe if sensor stops working
