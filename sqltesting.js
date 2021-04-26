59730ffb6310abb4e33b7dde2ca46fb5e6793540
Kent Roth <kentencroth@yahoo.com>
2021-04-26 17:38:41 -0500

    sql testing things

---
+++ sqltesting.js
@@ -0,0 +1,36 @@
+CREATE TABLE planters(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    planter INT,
+    plant VARCHAR(50) NOT null,
+    planted Date DEFAULT CURDATE()
+);
+
+CREATE TABLE sensors(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    messurment DECIMAL(6,2),
+    taken DATETIME DEFAULT NOW(),
+    planter_id INT,
+    FOREIGN KEY(planter_id) REFERENCES planters(id)
+);
+
+CREATE TABLE waterMotors(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    watered DATETIME DEFAULT NOW(),
+    planter_id INT,
+    sensor_id INT,
+    FOREIGN KEY(planter_id) REFERENCES planters(id),
+    FOREIGN KEY(sensor_id) REFERENCES sensors(id)
+);
+
+INSERT INTO planters (planter, plant)
+    VALUES (1, 'Garlic');
+
+INSERT INTO planters (planter, plant)
+    VALUES (2, 'Green Onions');
+
+INSERT INTO sensors (messurment, planter_id)
+    VALUES (1892.39, 1);
+
+INSERT INTO waterMotors (planter_id, sensor_id) 
+    VALUES(1, LAST_INSERT_ID())
+
