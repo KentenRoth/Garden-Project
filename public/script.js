fa89cc16e99801f59a869cd8adda73d974f7aec1
Kent Roth <kentencroth@yahoo.com>
2021-05-03 12:38:05 -0500

    Setting up websockets

--- public/script.js
+++ public/script.js
@@ -3,3 +3,13 @@ connected = () => {
 };
 
 connected();
+
+const socket = new WebSocket('ws://localhost:3000');
+
+socket.addEventListener('open', function (event) {
+	console.log('connected to websocket server');
+});
+
+socket.addEventListener('message', function (event) {
+	console.log('message from server ', event.data);
+});
--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const routes = require('./routes');
+const WebSocket = require('ws');
 
 const app = express();
+const server = require('http').createServer(app);
 const port = 3000;
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', function connection(ws) {
+	console.log('Connected!!!');
+	ws.send('Welcome');
+
+	ws.on('message', function incoming(message) {
+		console.log(message);
+	});
+});
 
 app.use(express.json());
 
 app.use('/garden', routes);
 
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`Connected on port ${port}`);
 });
