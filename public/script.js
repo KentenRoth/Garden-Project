a03f868bea878a57638f132accd37a999d3c0124
Kent Roth <kentencroth@yahoo.com>
2021-05-08 11:01:02 -0500

    Connecting front end

--- public/script.js
+++ public/script.js
@@ -12,15 +12,43 @@ socket.addEventListener('message', function (event) {
 getWateringData = (num) => {
 	axios
 		.get(url + `/waterMotors/${num}`)
-		.then((data) => console.log(data.data[0]))
+		.then((data) => {
+			console.log(data.data[0]);
+			setPlant(data.data[0].plant, num);
+			setWatered(data.data[0].time, num);
+		})
 		.catch((err) => console.log(err));
 };
 
 getSensorData = (num) => {
 	axios
 		.get(url + `/${num}`)
-		.then((data) => console.log(data.data[0]))
+		.then((data) => {
+			console.log(data);
+			setMeasurement(data.data[0].messurment, num);
+		})
 		.catch((err) => console.log(err));
+};
+
+setPlant = (plant, num) => {
+	const plantedPlanter = document.getElementById(`planted${num}`);
+	const planted = document.createElement('p');
+	planted.textContent = `Plant: ${plant}`;
+	plantedPlanter.appendChild(planted);
+};
+
+setMeasurement = (measure, num) => {
+	const measurementPlanter = document.getElementById(`measurement${num}`);
+	const measurement = document.createElement('p');
+	measurement.textContent = `Measurement: ${measure}`;
+	measurementPlanter.appendChild(measurement);
+};
+
+setWatered = (watered, num) => {
+	const wateredPlanter = document.getElementById(`watered${num}`);
+	const wateredDate = document.createElement('p');
+	wateredDate.textContent = `Last Watered: ${watered}`;
+	wateredPlanter.appendChild(wateredDate);
 };
 
 getWateringData(1);
