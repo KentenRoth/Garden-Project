6163db5eead575a425e300fa473882e52482c79b
Kent Roth <kentencroth@yahoo.com>
2021-05-08 11:02:31 -0500

    Realized I cannot spell....

--- sqltesting.sql
+++ sqltesting.sql
@@ -7,7 +7,7 @@ CREATE TABLE planters(
 
 CREATE TABLE sensors(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    messurment DECIMAL(6,2),
+    measurement DECIMAL(6,2),
     taken DATETIME DEFAULT NOW(),
     planter_id INT,
     FOREIGN KEY(planter_id) REFERENCES planters(id)
@@ -28,33 +28,33 @@ INSERT INTO planters (planter, plant)
 INSERT INTO planters (planter, plant)
     VALUES (2, 'Green Onions');
 
-INSERT INTO sensors (messurment, planter_id)
+INSERT INTO sensors (measurement, planter_id)
     VALUES (1892.39, 1);
 
 INSERT INTO waterMotors (planter_id, sensor_id) 
     VALUES(1, LAST_INSERT_ID())
 
 
--- Gets last time watered, for what plant, and the messurment it was at
-SELECT plant, messurment, watered FROM waterMotors
+-- Gets last time watered, for what plant, and the measurement it was at
+SELECT plant, measurement, watered FROM waterMotors
     INNER JOIN sensors ON waterMotors.sensor_id = sensors.id
     INNER JOIN planters ON sensors.planter_id = planters.id
     WHERE waterMotors.planter_id = 1
     ORDER BY watered DESC LIMIT 1;
 
-    SELECT plant, messurment, watered FROM waterMotors
+    SELECT plant, measurement, watered FROM waterMotors
     INNER JOIN sensors ON waterMotors.sensor_id = sensors.id
     INNER JOIN planters ON sensors.planter_id = planters.id
     WHERE waterMotors.planter_id = 2
     ORDER BY watered DESC LIMIT 1;
 
--- Gets the last messurment or each plant
-SELECT plant, messurment, taken FROM sensors
+-- Gets the last measurement or each plant
+SELECT plant, measurement, taken FROM sensors
     INNER JOIN planters on sensors.planter_id = planters.id
     WHERE  planter_id = 1
     ORDER BY taken DESC LIMIT 1;
 
-SELECT plant, messurment, taken FROM sensors
+SELECT plant, measurement, taken FROM sensors
     INNER JOIN planters on sensors.planter_id = planters.id
     WHERE planter_id = 2
     ORDER BY taken DESC LIMIT 1
