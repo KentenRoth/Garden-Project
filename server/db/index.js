67d8091ab49e68adc6c5a3a5c7609b60d3858b9e
Kent Roth <kentencroth@yahoo.com>
2021-05-16 06:44:36 -0500

    fixed spelling error

--- server/db/index.js
+++ server/db/index.js
@@ -11,7 +11,7 @@ let gardendb = {};
 
 gardendb.sensorsData = (planter_id) => {
 	const q =
-		"SELECT messurment, planter_id, DATE_FORMAT(taken, '%m/%d/%y %H:%i') AS time FROM sensors\
+		"SELECT measurement, planter_id, DATE_FORMAT(taken, '%m/%d/%y %H:%i') AS time FROM sensors\
         WHERE planter_id = ? ORDER BY taken DESC LIMIT 1";
 	return new Promise((resolve, reject) => {
 		con.query(q, [planter_id], function (err, results) {
@@ -24,12 +24,12 @@ gardendb.sensorsData = (planter_id) => {
 };
 
 let lastID;
-gardendb.insertSensorData = (messurment, planter_id) => {
+gardendb.insertSensorData = (measurement, planter_id) => {
 	const q =
-		'INSERT INTO sensors (messurment, planter_id)\
+		'INSERT INTO sensors (measurement, planter_id)\
         VALUES ( ?, ?)';
 	return new Promise((resolve, reject) => {
-		con.query(q, [messurment, planter_id], function (err, results) {
+		con.query(q, [measurement, planter_id], function (err, results) {
 			if (err) {
 				return reject(err);
 			}
@@ -41,7 +41,7 @@ gardendb.insertSensorData = (messurment, planter_id) => {
 
 gardendb.waterMotorData = (planter_id) => {
 	const q =
-		"SELECT plant, messurment, DATE_FORMAT(watered, '%m/%d/%y %H:%i') AS time FROM waterMotors\
+		"SELECT plant, measurement, DATE_FORMAT(watered, '%m/%d/%y %H:%i') AS time FROM waterMotors\
         INNER JOIN sensors ON waterMotors.sensor_id = sensors.id\
         INNER JOIN planters ON sensors.planter_id = planters.id\
         WHERE waterMotors.planter_id = ?\
@@ -71,7 +71,5 @@ gardendb.insertWaterData = (planter_id) => {
 		});
 	});
 };
-
-// Fix spelling issue messurment is spelled measurement
 
 module.exports = gardendb;
