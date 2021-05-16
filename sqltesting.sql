f993d096ef7afdefe902d7d4ccac0de8ed793b2c
Kent Roth <kentencroth@yahoo.com>
2021-05-16 06:54:22 -0500

    added comments

--- sqltesting.sql
+++ sqltesting.sql
@@ -22,18 +22,16 @@ CREATE TABLE waterMotors(
     FOREIGN KEY(sensor_id) REFERENCES sensors(id)
 );
 
+-- Currently this is what's planted.
 INSERT INTO planters (planter, plant)
-    VALUES (1, 'Garlic');
+    VALUES (1, 'Radishes'),
+            (2, 'Green Onions')
 
-INSERT INTO planters (planter, plant)
-    VALUES (2, 'Green Onions');
-
-INSERT INTO sensors (measurement, planter_id)
-    VALUES (1892.39, 1);
-
+-- Instead of LAST_INSERT_ID
+-- I am returning the insertID and passing it in
+-- LAST_INSERT_ID was giving to many issues
 INSERT INTO waterMotors (planter_id, sensor_id) 
     VALUES(1, LAST_INSERT_ID())
-
 
 -- Gets last time watered, for what plant, and the measurement it was at
 SELECT plant, measurement, watered FROM waterMotors
@@ -48,7 +46,7 @@ SELECT plant, measurement, watered FROM waterMotors
     WHERE waterMotors.planter_id = 2
     ORDER BY watered DESC LIMIT 1;
 
--- Gets the last measurement or each plant
+-- Gets the last measurement for each plant
 SELECT plant, measurement, taken FROM sensors
     INNER JOIN planters on sensors.planter_id = planters.id
     WHERE  planter_id = 1
